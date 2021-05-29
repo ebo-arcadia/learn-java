87656666f45b035f96cbf90c6fd26fdb6c1f5a56
Ebo Lee <ebolee@Ebos-MacBook-Air.local>
2021-05-29 11:25:28 -0700

    using single thread

---
+++ multithreading.java
@@ -0,0 +1,10 @@
+// process (instances) assigned to either single or multiple threads
+// thread class
+// user thread & daemon thread
+
+// single thread example
+class single_thread {
+    public static void main(String[] args) {
+        System.out.println("This is a single thread");
+    }
+}
