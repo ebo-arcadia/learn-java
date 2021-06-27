f623f6c0b877057b477a9b5b2df8a89f6932a834
Ebo Lee <yiboli.coding@gmail.com>
2021-06-26 21:43:42 -0700

    understanding default methods in interface & evolving interfaces

--- oracle_java_tutorials/interfaces.java
+++ oracle_java_tutorials/interfaces.java
@@ -123,3 +123,98 @@ public Object findLargest(Object object1, Object object2) {
        return object2;
  }
 
+ // evolving interfaces
+
+ public interface DoIt {
+    void doSomething(int i, double x);
+    int doSomethingElse(String s);
+ }
+
+ // if user to add a another method to it
+ public interface DoIt {
+
+    void doSomething(int i, double x);
+    int doSomethingElse(String s);
+    boolean didItWork(int i, double x, String s);
+    
+ }
+
+ // classes implement old inferace will break
+// solution
+public interface DoIt {
+
+    void doSomething(int i, double x);
+    int doSomethingElse(String s);
+    default boolean didItWork(int i, double x, String s) {
+        // Method body 
+    }
+    
+ }
+
+ // default methods
+//  enable adding functionality to interfaces binary compatibility for older version
+import java.time.*; 
+ 
+public interface TimeClient {
+    void setTime(int hour, int minute, int second);
+    void setDate(int day, int month, int year);
+    void setDateAndTime(int day, int month, int year,
+                               int hour, int minute, int second);
+    LocalDateTime getLocalDateTime();
+}
+
+package defaultmethods;
+
+import java.time.*;
+import java.lang.*;
+import java.util.*;
+
+public class SimpleTimeClient implements TimeClient {
+    
+    private LocalDateTime dateAndTime;
+    
+    public SimpleTimeClient() {
+        dateAndTime = LocalDateTime.now();
+    }
+    
+    public void setTime(int hour, int minute, int second) {
+        LocalDate currentDate = LocalDate.from(dateAndTime);
+        LocalTime timeToSet = LocalTime.of(hour, minute, second);
+        dateAndTime = LocalDateTime.of(currentDate, timeToSet);
+    }
+    
+    public void setDate(int day, int month, int year) {
+        LocalDate dateToSet = LocalDate.of(day, month, year);
+        LocalTime currentTime = LocalTime.from(dateAndTime);
+        dateAndTime = LocalDateTime.of(dateToSet, currentTime);
+    }
+    
+    public void setDateAndTime(int day, int month, int year,
+                               int hour, int minute, int second) {
+        LocalDate dateToSet = LocalDate.of(day, month, year);
+        LocalTime timeToSet = LocalTime.of(hour, minute, second); 
+        dateAndTime = LocalDateTime.of(dateToSet, timeToSet);
+    }
+    
+    public LocalDateTime getLocalDateTime() {
+        return dateAndTime;
+    }
+    
+    public String toString() {
+        return dateAndTime.toString();
+    }
+    
+    public static void main(String... args) {
+        TimeClient myTimeClient = new SimpleTimeClient();
+        System.out.println(myTimeClient.toString());
+    }
+}
+
+public interface TimeClient {
+    void setTime(int hour, int minute, int second);
+    void setDate(int day, int month, int year);
+    void setDateAndTime(int day, int month, int year,
+        int hour, int minute, int second);
+    LocalDateTime getLocalDateTime();                           
+    ZonedDateTime getZonedDateTime(String zoneString);
+}
